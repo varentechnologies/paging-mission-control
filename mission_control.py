48ca78e11fb7965a7c079c3f717d2c10c8562f4e
David Primm <dsprimm1@hotmail.com>
2020-02-10 11:43:39 -0500

    finish documentation, correct for pep8 and pylint

--- mission_control.py
+++ mission_control.py
@@ -7,6 +7,7 @@ module docstring for mission_control
 import time
 import argparse
 
+
 class AlertGeneration():
     """
     class AlertGeneration()
@@ -17,8 +18,8 @@ class AlertGeneration():
     def __init__(self):
         self.alerted = {}
         self.count = 0
-        self.output = \
-'''     satelliteId: {0},
+        self.output = '''
+        satelliteId: {0},
         severity: "{1}",
         component: "{2}",
         timestamp: "{3}"'''
@@ -34,34 +35,49 @@ class AlertGeneration():
         return a 1, on success it will return a 0
         """
         if self.alerted.get(id_to_add, None) is None:
-            self.alerted[id_to_add] = {"BATT":[], "TSTAT":[]}
+            self.alerted[id_to_add] = {"BATT": [], "TSTAT": []}
             return 0
         return 1
 
-    def time_check(self, id_to_check, component, _time):
+    def time_check(self, check_id, component, _time):
         """
-        time_check(self, str id_to_check, str, component, str _time)
+        time_check(self, str check_id, str, component, str _time)
 
+        time_check checks to see if the current alert time is within 5 minutes
+        of alerts that have already been registered, in the event that it is
+        outside of the time window, it will pop off the oldest element of its
+        list and recursively call again, if it is within the time window, the
+        alert will be added to its list, in the event of its list being empty
+        the alert will be added to its list
         """
         time_value = time.mktime(time.strptime(_time[:-4], "%Y%m%d %H:%M:%S"))
-        if len(self.alerted[id_to_check][component]) > 0:
-            if time_value - self.alerted[id_to_check][component][0][0] > 300:
-                self.alerted[id_to_check][component].pop(0)
-                self.time_check(id_to_check, component, _time)
+        if len(self.alerted[check_id][component]) > 0:
+            if time_value - self.alerted[check_id][component][0][0] > 300:
+                self.alerted[check_id][component].pop(0)
+                self.time_check(check_id, component, _time)
             else:
-                self.alerted[id_to_check][component].append([time_value, _time])
+                self.alerted[check_id][component].append([time_value, _time])
         else:
-            self.alerted[id_to_check][component].append([time_value, _time])
+            self.alerted[check_id][component].append([time_value, _time])
 
-    def check(self, high_limit, low_limit, value, component, id_to_work, _time):
+    def check(self, high_limit, low_limit, value, component, work_id, _time):
         """
         check(self, str high_limit, str low_limit,
-              str value, str component, str ID, str _time)
+              str value, str component, str work_id, str _time)
+
+        check takes in the limits, value, component, id, and timestamp, if the
+        id is not in the dictionary self.alerted self.add_id will be called to
+        populate the dictionary, based on the component, the value will be
+        checked according to the requirred paramaters, in the event that a
+        warning has been flagged, self.time_check will check the alerts and
+        handle according to its results, finally it will check to see if the
+        alert dictionary for the component has over 2 entries, in the event it
+        does, self.printer will be called to output the formatted data
 
         """
         warn = 0
-        if self.alerted.get(id_to_work, None) is None:
-            self.add_id(id_to_work)
+        if self.alerted.get(work_id, None) is None:
+            self.add_id(work_id)
         if component == "BATT":
             if float(value) < float(low_limit):
                 warn = 1
@@ -69,13 +85,18 @@ class AlertGeneration():
             if float(value) > float(high_limit):
                 warn = 1
         if warn == 1:
-            self.time_check(id_to_work, component, _time)
-        if len(self.alerted[id_to_work][component]) > 2:
-            self.printer(id_to_work, component)
+            self.time_check(work_id, component, _time)
+        if len(self.alerted[work_id][component]) > 2:
+            self.printer(work_id, component)
 
     def printer(self, id_to_print, component):
         """
         printer(self, str id_to_print, str component)
+
+        printer will pop the oldest element of the alert list, and output the
+        data in a JSON format according to the requirred specifications, as
+        there was no specified way to handle the remaining data, it is left
+        in place
         """
         if self.count == 0:
             print("[")
@@ -92,25 +113,22 @@ class AlertGeneration():
         print("    {")
         print(self.output.format(ID, severity, component, timestamp))
 
-    def end(self):
-        """
-        end(self)
-        """
-        print("    }")
-        print("]")
 
 if __name__ == "__main__":
-    PARSER = argparse.ArgumentParser(description='')
-    PARSER.add_argument('input_file', metavar='F', type=str,
-                        help='')
+    PARSER = argparse.ArgumentParser(description='''
+    mission_control takes a correctly formatted input file and outputs a JSON
+    style alerts
+    ''')
+    PARSER.add_argument('input_file', type=str, help='file to parse')
     ARGS = PARSER.parse_args()
     ALERT_GENERATOR = AlertGeneration()
     with open(ARGS.input_file, "r") as in_file:
         while True:
             LINE = in_file.readline().strip("\n")
             if LINE:
-                TIME, ID, H_LIMIT, ___, L_LIMIT, __, VALUE, COMPOENT = LINE.split("|")
-                ALERT_GENERATOR.check(H_LIMIT, L_LIMIT, VALUE, COMPOENT, ID, TIME)
+                TIME, ID, HIGH, ___, LOW, __, VALUE, COMP = LINE.split("|")
+                ALERT_GENERATOR.check(HIGH, LOW, VALUE, COMP, ID, TIME)
             else:
+                print("    }")
+                print("]")
                 break
-        ALERT_GENERATOR.end()
