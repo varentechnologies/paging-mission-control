03908f024e91dbf0efa9e0b9fa8b0a9eb16a96c1
Brian Hart <brian.hart@xylogix.net>
2021-03-20 22:02:48 -0400

    Create FormatTimeString class

---
+++ PagnigMissionControl/PagnigMissionControl.Formatters/FormatTimeString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PagnigMissionControl.Formatters
+{
+    /// <summary>Formats timestamp strings according to spec.</summary>
+    public static class FormatTimeString
+    {
+        /// <summary>Given the <paramref name="timestamp" /> string provided (ostensibly from the satellite raw data), reformats it per JSON spec and then returns the result.</summary>
+        /// <param name="timestamp">(Required.) String containing the timestamp from the input data.</param>
+        /// <returns>String containing the timestamp formatted per spec.</returns>
+        public static string FromTimestamp(string timestamp)
+            => DateTime.Parse(
+                           timestamp.Insert(4, "-")
+                                    .Insert(7, "-")
+                                    .Replace(" ", "T")
+                       )
+                       .ToString("yyyy-MM-ddTHH:mm:ss.fff");
+    }
+}
