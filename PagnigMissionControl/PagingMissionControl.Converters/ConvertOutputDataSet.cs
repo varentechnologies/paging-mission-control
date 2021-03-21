e2d46cde3d9f40edd1ff230f74985169da0488f5
Brian Hart <brian.hart@xylogix.net>
2021-03-20 23:08:06 -0400

    Create class 'ConvertOutputDataSet'

---
+++ PagnigMissionControl/PagingMissionControl.Converters/ConvertOutputDataSet.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using PagingMissionControl.Interfaces;
+using PagingMissionControl.Settings.Conversion;
+using System.Collections.Generic;
+
+namespace PagingMissionControl.Converters
+{
+    public static class ConvertOutputDataSet
+    {
+        public static string ToJson(IEnumerable<IOutputRow> row)
+            => JsonConvert.SerializeObject(
+                row, OutputRowConversionSettingsProvider.Settings
+            );
+    }
+}
