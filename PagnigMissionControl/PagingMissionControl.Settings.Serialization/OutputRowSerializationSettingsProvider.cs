84570004250bc155a9382fbe732a7772f8505f6c
Brian Hart <brian.hart@xylogix.net>
2021-03-20 22:29:45 -0400

    Create 'OutputRowSerializationSettingsProvider' class

---
+++ PagnigMissionControl/PagingMissionControl.Settings.Serialization/OutputRowSerializationSettingsProvider.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace PagingMissionControl.Settings.Serialization
+{
+    /// <summary>
+    /// Provides settings for serialization of objects to/from JSON.
+    /// </summary>
+    internal static class OutputRowSerializationSettingsProvider
+    {
+        /// <summary>
+        /// Gets the settings for serialization of objects to/from JSON.
+        /// </summary>
+        public static readonly JsonSerializerSettings Settings =
+            new JsonSerializerSettings
+            {
+                MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+                Formatting = Formatting.Indented,                               // pretty print
+                DateParseHandling = DateParseHandling.None
+            };
+    }
+}
