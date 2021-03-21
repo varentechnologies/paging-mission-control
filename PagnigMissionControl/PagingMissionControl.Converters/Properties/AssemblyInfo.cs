410cce246628728e523437a79618055f29a14ee7
Brian Hart <brian.hart@xylogix.net>
2021-03-20 23:06:12 -0400

    Rename project using automated tool
    The tool is on GitHub at:
    https://github.com/astrohart/MassFileRenamer

--- PagnigMissionControl/PagingMissionControl.Converters/Properties/AssemblyInfo.cs
+++ PagnigMissionControl/PagingMissionControl.Converters/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PagingMissionControl.Serializers")]
+[assembly: AssemblyTitle("PagingMissionControl.Converters")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PagingMissionControl.Serializers")]
+[assembly: AssemblyProduct("PagingMissionControl.Converters")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
