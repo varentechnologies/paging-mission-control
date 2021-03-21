e761fd3612d4085374e238410df0705f99447f29
Brian Hart <brian.hart@xylogix.net>
2021-03-20 20:42:23 -0400

    Create IInputRow interface and Component constant class

---
+++ PagnigMissionControl/PagingMissionControl.Constants/Components.cs
@@ -0,0 +1,16 @@
+﻿namespace PagingMissionControl.Constants
+{
+    /// <summary>Constants that label the components of the satellites.</summary>
+    public static class Component
+    {
+        /// <summary>
+        /// The component referenced is the satellite's battery.
+        /// </summary>
+        public const string Battery = "BATT";
+
+        /// <summary>
+        /// The component referenced is the satellite's thermostat.
+        /// </summary>
+        public const string Thermostat = "TSTAT";
+    }
+}
