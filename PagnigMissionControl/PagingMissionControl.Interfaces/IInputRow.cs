31a4925bfd3eda5d6d9737e28396c11de3526f14
Brian Hart <brian.hart@xylogix.net>
2021-03-20 21:52:10 -0400

    Create IOutputRow and reformat code

--- PagnigMissionControl/PagingMissionControl.Interfaces/IInputRow.cs
+++ PagnigMissionControl/PagingMissionControl.Interfaces/IInputRow.cs
@@ -1,79 +1,43 @@
 ﻿namespace PagingMissionControl.Interfaces
 {
-    /// <summary>
-    /// Defines the methods and properties of an object that encapsulates the
-    /// data elements in each input record.
-    /// </summary>
+    /// <summary>Defines the methods and properties of an object that encapsulates the data elements in each input record.</summary>
     public interface IInputRow
     {
-        /// <summary>
-        /// Gets or sets a string that labels which component -- currently TSTAT
-        /// for Thermostat or BATT for Battery -- that this data refers to.
-        /// </summary>
+        /// <summary>Gets or sets a string that labels which component -- currently TSTAT for Thermostat or BATT for Battery -- that this data refers to.</summary>
         string Component { get; set; }
 
-        /// <summary>
-        /// Gets or sets the actual value of the observable quantity.
-        /// </summary>
+        /// <summary>Gets or sets the actual value of the observable quantity.</summary>
         decimal RawValue { get; set; }
 
-        /// <summary>
-        /// Gets or sets the value of the physical observable, above which or
-        /// at, puts the satellite in the high red-alert zone.
-        /// </summary>
-        /// <remarks>
-        /// This value is different for each particular record.
-        /// </remarks>f
+        /// <summary>Gets or sets the value of the physical observable, above which or at, puts the satellite in the high red-alert zone.</summary>
+        /// <remarks>This value is different for each particular record.</remarks>
+        /// f
         decimal RedHighLimit { get; set; }
 
-        /// <summary>
-        /// Gets or sets the value of the physical observable, lower than which
-        /// or at, puts the satellite in the low part of the red-alert zone.
-        /// </summary>
-        /// <remarks>
-        /// This value is different for each particular record.
-        /// <para/>
-        /// For example, if the voltage of the battery is at or below this
-        /// value, then the voltage is in the yellow-alert zone.
-        /// <para/>
-        /// Still lower, and the voltage now falls into the severely-low, or
-        /// red-alert low, zone.
-        /// </remarks>
+        /// <summary>Gets or sets the value of the physical observable, lower than which or at, puts the satellite in the low part of the red-alert zone.</summary>
+        /// <remarks>This value is different for each particular record.
+        /// <para />
+        /// For example, if the voltage of the battery is at or below this value, then the voltage is in the yellow-alert zone.
+        /// <para />
+        /// Still lower, and the voltage now falls into the severely-low, or red-alert low, zone.</remarks>
         decimal RedLowLimit { get; set; }
 
-        /// <summary>
-        /// Gets or sets an integer that specifies an identifier for the
-        /// particular satellite to which the current record pertains.
-        /// </summary>
+        /// <summary>Gets or sets an integer that specifies an identifier for the particular satellite to which the current record pertains.</summary>
         int SatelliteId { get; set; }
 
-        /// <summary>
-        /// Gets or sets a string containing the timestamp of the current entry.
-        /// </summary>
+        /// <summary>Gets or sets a string containing the timestamp of the current entry.</summary>
         string Timestamp { get; set; }
 
-        /// <summary>
-        /// Gets or sets the value of the physical observable, above which or
-        /// at, puts the satellite in the high yellow-alert zone.
-        /// </summary>
-        /// <remarks>
-        /// This value is different for each particular record.
-        /// </remarks>
+        /// <summary>Gets or sets the value of the physical observable, above which or at, puts the satellite in the high yellow-alert zone.</summary>
+        /// <remarks>This value is different for each particular record.</remarks>
         decimal YellowHighLimit { get; set; }
 
-        /// <summary>
-        /// Gets or sets the value of the physical observable, lower than which
-        /// or at, puts the satellite in the low part of the yellow-alert zone.
-        /// </summary>
-        /// <remarks>
-        /// This value is different for each particular record.
-        /// <para/>
-        /// For example, if the voltage of the battery is at or below this
-        /// value, then the voltage is in the yellow-alert zone.
-        /// <para/>
-        /// Still lower, and the voltage now falls into the severely-low, or
-        /// red-alert low, zone.
-        /// </remarks>
+        /// <summary>Gets or sets the value of the physical observable, lower than which or at, puts the satellite in the low part of the yellow-alert zone.</summary>
+        /// <remarks>This value is different for each particular record.
+        /// <para />
+        /// For example, if the voltage of the battery is at or below this value, then the voltage is in the yellow-alert zone.
+        /// <para />
+        /// Still lower, and the voltage now falls into the severely-low, or red-alert low, zone.</remarks>
         decimal YellowLowLimit { get; set; }
     }
 }
---
+++ PagnigMissionControl/PagingMissionControl.Interfaces/IOutputRow.cs
@@ -0,0 +1,18 @@
+﻿namespace PagingMissionControl.Interfaces
+{
+    /// <summary>Defines the publicly-exposed methods and properties of a single row of output data.</summary>
+    public interface IOutputRow
+    {
+        /// <summary>Gets or sets a string describing the component that is reporting the condition.</summary>
+        string Component { get; set; }
+
+        /// <summary>Gets or sets an integer that specifies an identifier for the particular satellite to which the current record pertains.</summary>
+        int SatelliteId { get; set; }
+
+        /// <summary>Gets or sets a string consisting of a description of the severity of a fault.</summary>
+        string Severity { get; set; }
+
+        /// <summary>Gets or sets a string containing the timestamp of the event.</summary>
+        string Timestamp { get; set; }
+    }
+}
